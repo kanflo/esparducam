95e160308ac08110025e9e24e89e42775b1a2186
Johan Kanflo <johan.kanflo@bitfuse.net>
2016-01-09 23:46:53 +0100

    Added camera power cycle Removed the 1s delay at start

--- capture.c
+++ capture.c
@@ -1,7 +1,7 @@
 /* 
  * The MIT License (MIT)
  * 
- * Copyright (c) 2015 Johan Kanflo (github.com/kanflo)
+ * Copyright (c) 2016 Johan Kanflo (github.com/kanflo)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -32,16 +32,15 @@
 #include <task.h>
 #include <spi.h>
 #include <i2c/i2c.h>
-#include "arducam.h"
+#include <http_upload.h>
+#include <arducam.h>
 #include <espressif/spi_flash.h>
-
 #include <espressif/sdk_private.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 #include <ssid_config.h>
 
-#include <http_upload.h>
 
 
 //#define CONFIG_NO_WIFI
@@ -198,6 +197,15 @@ void pir_task(void *p)
 void server_task(void *p)
 {
     bool camera_ok = false;
+
+    // Power cycle the the camera
+    gpio_enable(ARDUCAM_PWR, GPIO_OUTPUT);
+    printf(TIME_MARKER "Camera power cycle\n", systime_ms());
+    gpio_write(ARDUCAM_PWR, 1);
+    delay_ms(250);
+    gpio_write(ARDUCAM_PWR, 0);
+    delay_ms(250);
+
     camera_ok = arducam_setup();
     if (!camera_ok) {
         printf(TIME_MARKER "Camera init failed!\n", systime_ms()); 
@@ -284,7 +292,6 @@ void server_task(void *p)
 void user_init(void)
 {
     sdk_uart_div_modify(0, UART_CLK_FREQ / 115200);
-    delay_ms(1000);
     printf("\n\n\n");
     printf("SDK version:%s\n", sdk_system_get_sdk_version());
 
@@ -298,10 +305,6 @@ void user_init(void)
     sdk_wifi_set_opmode(STATION_MODE);
     sdk_wifi_station_set_config(&config);
 #endif // CONFIG_NO_WIFI
-
-    // Enable power to the camera
-    gpio_enable(ARDUCAM_PWR, GPIO_OUTPUT);
-    gpio_write(ARDUCAM_PWR, 1);
 
     xTaskCreate(&server_task, (signed char *) "server_task", 256, NULL, 2, NULL);
 #ifndef CONFIG_NO_PIR
